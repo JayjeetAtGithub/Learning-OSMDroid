6a91c136dc5023196e7f50bc0f002097e99e8134
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-02-16 12:03:03 +0530

    Zoom functionality fixed

--- app/src/main/java/com/phdetector/jayjeet/myapplication/RenderMapActivity.java
+++ app/src/main/java/com/phdetector/jayjeet/myapplication/RenderMapActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import org.osmdroid.bonuspack.kml.KmlDocument;
@@ -17,11 +18,12 @@ import java.io.File;
 import java.net.URL;
 import java.util.Map;
 
-public class RenderMapActivity extends AppCompatActivity {
+public class RenderMapActivity extends AppCompatActivity implements MapView.OnFirstLayoutListener {
 
     private String absPath;
     private MapView map;
     private KmlDocument kmlDocument;
+    private BoundingBox mInitialBoundingBox = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +43,27 @@ public class RenderMapActivity extends AppCompatActivity {
         kmlDocument.parseKMLFile(file);
 
         FolderOverlay kmlOverlay = (FolderOverlay)kmlDocument.mKmlRoot.buildOverlay(map, null, null, kmlDocument);
-
-        BoundingBox bb = kmlDocument.mKmlRoot.getBoundingBox();
-        map.zoomToBoundingBox(bb,true);
-        map.getController().setCenter(bb.getCenter());
         map.getOverlays().add(kmlOverlay);
         map.invalidate();
+        BoundingBox bb = kmlDocument.mKmlRoot.getBoundingBox();
+        if(bb != null){
+            setInitialViewOn(bb);
+        }
+    }
 
+
+
+    void setInitialViewOn(BoundingBox bb) {
+        if (map.getScreenRect(null).height() == 0) {
+            mInitialBoundingBox = bb;
+            map.addOnFirstLayoutListener(this);
+        } else
+            map.zoomToBoundingBox(bb, false);
+    }
+
+    @Override
+    public void onFirstLayout(View v, int left, int top, int right, int bottom) {
+        if (mInitialBoundingBox != null)
+            map.zoomToBoundingBox(mInitialBoundingBox, false);
     }
 }
