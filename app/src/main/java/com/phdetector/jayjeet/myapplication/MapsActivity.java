b5d39ea94d8bcbadb66da455660dddea01c0a182
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-02-20 14:23:56 +0530

    Changed Polyline to Polygon

--- app/src/main/java/com/phdetector/jayjeet/myapplication/MapsActivity.java
+++ app/src/main/java/com/phdetector/jayjeet/myapplication/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.phdetector.jayjeet.myapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,7 +30,7 @@ public class MapsActivity extends AppCompatActivity implements MapEventsReceiver
     private MapEventsOverlay mapEventsOverlay;
     private KmlDocument kmlDocument;
     private List<GeoPoint> geoPoints;
-    private Polyline polyline;
+    private Polygon polygon;
     private Button saveButton;
 
     @Override
@@ -42,12 +43,12 @@ public class MapsActivity extends AppCompatActivity implements MapEventsReceiver
             @Override
             public void onClick(View v) {
                 // Saves the polygon in a KML File
-                kmlDocument.mKmlRoot.addOverlay(polyline,kmlDocument);
+                kmlDocument.mKmlRoot.addOverlay(polygon,kmlDocument);
                 long millis = System.currentTimeMillis();
                 File localFile = kmlDocument.getDefaultPathForAndroid("KML_" + millis + ".kml");
                 Log.d("path",localFile.getAbsolutePath());
                 kmlDocument.saveAsKML(localFile);
-                Toast.makeText(getApplicationContext(),"Polyline Saved",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"Polygon Saved",Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -66,21 +67,21 @@ public class MapsActivity extends AppCompatActivity implements MapEventsReceiver
         map.getOverlays().add(0, mapEventsOverlay);
 
         geoPoints = new ArrayList<>();
-        polyline = new Polyline();
-
+        polygon = new Polygon();
+        polygon.setFillColor(Color.argb(75, 255,0,0));
     }
 
     @Override
     public boolean singleTapConfirmedHelper(GeoPoint p) {
         geoPoints.add(p);
-        polyline.setPoints(geoPoints);
+        polygon.setPoints(geoPoints);
         Marker marker = new Marker(map);
         marker.setPosition(p);
         //marker.setIcon(getResources().getDrawable(R.mipmap.marker_red_round));
         marker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_BOTTOM);
         marker.setTitle(String.valueOf(p.getLatitude() + "," + p.getLongitude()));
         map.getOverlays().add(marker);
-        map.getOverlayManager().add(polyline);
+        map.getOverlayManager().add(polygon);
         map.invalidate();
         return true;
     }
